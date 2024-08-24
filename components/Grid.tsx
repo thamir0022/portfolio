714c4e745b196190a7c7e2ac1f40078175c6f540
Thamir <thamirsiddik@gmail.com>
2024-08-24 21:48:48 +0530

    Implement Hero and Grid Section

---
+++ components/Grid.tsx
@@ -0,0 +1,16 @@
+import { gridItems } from "@/data";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+
+const Grid = () => {
+  return (
+    <section id="about">
+      <BentoGrid>
+        {gridItems.map(({className, description, id, img, imgClassName, spareImg, title, titleClassName}) => (
+          <BentoGridItem className={className} id={id} key={id} title={title} description={description} img={img} spareImg={spareImg} imgClassName={imgClassName}/>
+        ))}
+      </BentoGrid>
+    </section>
+  );
+};
+
+export default Grid;
