c3b0492d3fe22161517b2180d3ac53cb23b1f777
Thamir <thamirsiddik@gmail.com>
2024-09-01 22:15:17 +0530

    Implement google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,13 @@ import type { Metadata } from "next";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Thamir S | Next.js Developer Portfolio",
-  description: "Explore the portfolio of Thamir S, a passionate Next.js developer specializing in modern, scalable web applications. Discover projects showcasing expertise in React, Next.js, and cutting-edge web technologies.",
+  description:
+    "Explore the portfolio of Thamir S, a passionate Next.js developer specializing in modern, scalable web applications. Discover projects showcasing expertise in React, Next.js, and cutting-edge web technologies.",
 };
-
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-9ND17WDZD3"
+        ></script>
+        <script
+          id="google-analytics"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-9ND17WDZD3');
+            `,
+          }}
+        />
+      </Head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
